76f64d8e0185f89b6cda08aa6410538a8fd2eb87
Strencher <46447572+Strencher@users.noreply.github.com>
2021-07-04 14:48:34 -0700

    Update js.js

--- core/loaders/js.js
+++ core/loaders/js.js
@@ -6,7 +6,7 @@ export default function js() {
             loader: resolveModule("babel-loader"),
             options: {
                 presets: [
-                    [resolveModule("@babel/typescript"), {esModuleInterop: true}]
+                    [resolveModule("@babel/preset-typescript"), {esModuleInterop: true}]
                 ],
                 plugins: [
                     "@babel/plugin-transform-react-jsx",
